e20d23fc025c74c0406787be557003fdec59194d
Paul Kendall <pkendall64@gmail.com>
2022-07-16 09:20:42 +1200

    eslink the webpack files

--- webpack.common.js
+++ webpack.common.js
@@ -1,45 +1,45 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require('terser-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './src/js/app.js',
-    output: {
-        path: `${__dirname}/dist`,
-        filename: 'bundle.js',
-        publicPath: '',
+  entry: './src/js/app.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: ''
+  },
+  resolve: {
+    alias: {
+      node_modules: path.join(__dirname, 'node_modules')
     },
-    resolve: {
-        alias: {
-            'node_modules': path.join(__dirname, 'node_modules'),
-        },
-        fallback: {
-            'crypto': false,
-        }
-    },
-    plugins: [
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
-        ],
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                extractComments: false,
-            }),
-            new CssMinimizerPlugin(),
-        ],
+    fallback: {
+      crypto: false
     }
-};
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false
+      }),
+      new CssMinimizerPlugin()
+    ]
+  }
+}
