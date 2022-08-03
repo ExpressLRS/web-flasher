8aa22b508755e47d0f933f0501655e411b16f17c
Justin <38869875+justinlampley@users.noreply.github.com>
2022-08-03 17:39:24 -0400

    use await instead of promise chaining and use string templates

--- src/js/passthrough.js
+++ src/js/passthrough.js
@@ -69,20 +69,18 @@ class Passthrough {
     this.uploadforce = uploadforce
   }
 
-  _validate_serialrx = (config, expected) => {
-    return this.transport.write_string('get ' + config + '\r\n')
-      .then(_ => this.transport.read_line({ timeout: 100 }))
-      .then(line => {
-        console.log(line)
-        for (const key of expected) {
-          if (line.trim().indexOf(' = ' + key) !== -1) {
-            console.log('found')
-            return true
-          }
-        }
-        console.log('NOT found')
-        return false
-      })
+  _validate_serialrx = async (config, expected) => {
+    await this.transport.write_string(`get ${config}\r\n`)
+    const line = await this.transport.read_line({ timeout: 100 })
+    console.log(line)
+    for (const key of expected) {
+      if (line.trim().indexOf(` = ${key}`) !== -1) {
+        console.log('found')
+        return true
+      }
+    }
+    console.log('NOT found')
+    return false
   }
 
   _sleep (ms) {
@@ -93,180 +91,170 @@ class Passthrough {
     this.terminal.writeln(str)
   }
 
-  edgeTX = () => {
+  edgeTX = async () => {
     this.log('======== PASSTHROUGH INIT ========')
 
-    const sendExpect = (send, expect, delay) => this.transport.write_string(send)
-      .then(_ => this.transport.read_line({ timeout: 100 }))
-      .then(line => {
-        if (line.indexOf(expect) === -1) {
-          throw new AlertError('Failed Passthrough Initialisation', `Wanted '${expect}', but not found in response '${line}'`)
-        }
-      })
-      .then(_ => this._sleep(delay))
+    const sendExpect = async (send, expect, delay) => {
+      await this.transport.write_string(send)
+      const line = await this.transport.read_line({ timeout: 100 })
+
+      if (line.indexOf(expect) === -1) {
+        throw new AlertError('Failed Passthrough Initialisation', `Wanted '${expect}', but not found in response '${line}'`)
+      }
+      await this._sleep(delay)
+    }
 
     this.transport.set_delimiters(['> '])
-    return sendExpect('set pulses 0\n', 'set: ', 500)
-      .then(_ => sendExpect('set rfmod 0 power off\n', 'set: ', 500))
-      .then(_ => sendExpect('set rfmod 0 bootpin 1\n', 'set: ', 100))
-      .then(_ => sendExpect('set rfmod 0 power on\n', 'set: ', 100))
-      .then(_ => sendExpect('set rfmod 0 bootpin 0\n', 'set: ', 0))
-      .then(_ => {
-        this.log('Enabling serial passthrough...')
-        this.transport.set_delimiters(['\n'])
-        const cmd = 'serialpassthrough rfmod 0 ' + this.transport.baudrate.toString()
-        this.log(`  CMD: '${cmd}`)
-        return this.transport.write_string(cmd + '\n')
-          .then(_ => this.transport.read_line({ timeout: 200 }))
-      })
-      .then(_ => this.log('======== PASSTHROUGH DONE ========'))
-      .catch(e => {
-        this.log(e.message)
-        this.log('======== PASSTHROUGH FAILED ========')
-        return Promise.reject(e)
-      })
+    try {
+      await sendExpect('set pulses 0\n', 'set: ', 500)
+      await sendExpect('set rfmod 0 power off\n', 'set: ', 500)
+      await sendExpect('set rfmod 0 bootpin 1\n', 'set: ', 100)
+      await sendExpect('set rfmod 0 power on\n', 'set: ', 100)
+      await sendExpect('set rfmod 0 bootpin 0\n', 'set: ', 0)
+
+      this.log('Enabling serial passthrough...')
+      this.transport.set_delimiters(['\n'])
+      const cmd = `serialpassthrough rfmod 0 ${this.transport.baudrate.toString()}`
+      this.log(`  CMD: '${cmd}`)
+      await this.transport.write_string(`${cmd}\n`)
+      await this.transport.read_line({ timeout: 200 })
+
+      this.log('======== PASSTHROUGH DONE ========')
+    } catch (e) {
+      this.log(e.message)
+      this.log('======== PASSTHROUGH FAILED ========')
+      return Promise.reject(e)
+    }
   }
 
-  betaflight = () => {
+  betaflight = async () => {
     this.log('======== PASSTHROUGH INIT ========')
 
-    return this.transport.write_string('#\r\n')
-      .then(_ => {
-        this.transport.set_delimiters(['# ', 'CCC'])
-        return this.transport.read_line({ timeout: 200 })
-      })
-      .then(line => {
-        if (line.indexOf('CCC') !== -1) {
-          this.log('Passthrough already enabled and bootloader active')
-          return
-        }
-        if (!line.trim().endsWith('#')) {
-          this.log('No CLI available. Already in passthrough mode?, If this fails reboot FC and try again!')
-          return
-        }
+    await this.transport.write_string('#\r\n')
+    this.transport.set_delimiters(['# ', 'CCC'])
+    const line = await this.transport.read_line({ timeout: 200 })
+    if (line.indexOf('CCC') !== -1) {
+      this.log('Passthrough already enabled and bootloader active')
+      return
+    }
+    if (!line.trim().endsWith('#')) {
+      this.log('No CLI available. Already in passthrough mode?, If this fails reboot FC and try again!')
+      return
+    }
 
-        this.transport.set_delimiters(['# '])
+    this.transport.set_delimiters(['# '])
 
-        let waitfor = []
-        if (this.half_duplex) {
-          waitfor = ['GHST']
-        } else {
-          waitfor = ['CRSF', 'ELRS']
+    let waitfor = []
+    if (this.half_duplex) {
+      waitfor = ['GHST']
+    } else {
+      waitfor = ['CRSF', 'ELRS']
+    }
+    const serialCheck = []
+
+    if (!await this._validate_serialrx('serialrx_provider', waitfor)) {
+      serialCheck.push('Serial Receiver Protocol is not set to CRSF! Hint: set serialrx_provider = CRSF')
+    }
+    if (!await this._validate_serialrx('serialrx_inverted', ['OFF'])) {
+      serialCheck.push('Serial Receiver UART is inverted! Hint: set serialrx_inverted = OFF')
+    }
+    if (!await this._validate_serialrx('serialrx_halfduplex', ['OFF', 'AUTO'])) {
+      serialCheck.push('Serial Receiver UART is not in full duplex! Hint: set serialrx_halfduplex = OFF')
+    }
+    if (await this._validate_serialrx('rx_spi_protocol', ['EXPRESSLRS'])) {
+      serialCheck.push('ExpressLRS SPI RX detected\n\nUpdate via betaflight to flash your RX\nhttps://www.expresslrs.org/2.0/hardware/spi-receivers/')
+    }
+    if (serialCheck.length > 0) {
+      let msg = ''
+      this.log('[ERROR] Invalid serial RX configuration detected:\n')
+      for (const err of serialCheck) {
+        this.log(`    !!! ${err} !!!`)
+        msg += `${err}\n`
+      }
+      this.log('\n    Please change the configuration and try again!')
+      throw new AlertError('Invalid serial RX configuration detected', msg)
+    }
+
+    this.log('\nAttempting to detect FC UART configuration...')
+    await this.transport.write_string('serial\r\n')
+
+    this.transport.set_delimiters(['\n'])
+    let index = false
+    while (true) {
+      const line = await this.transport.read_line({ timeout: 200 })
+      if (line === '') {
+        break
+      }
+      if (line.startsWith('serial')) {
+        const regexp = /serial (?<port>[0-9]+) 64 /
+        const config = line.match(regexp)
+        if (config && config.groups && config.groups.port) {
+          index = config.groups.port
+          break
         }
-        const serialCheck = []
+      }
+    }
+    if (!index) {
+      this.log('!!! RX Serial not found !!!!\n  Check configuration and try again...')
+      throw new AlertError('Serial RX not found', 'Check flight controller RX configuration')
+    }
 
-        return this._validate_serialrx('serialrx_provider', waitfor)
-          .then(_ => { if (!_) serialCheck.push('Serial Receiver Protocol is not set to CRSF! Hint: set serialrx_provider = CRSF') })
-          .then(_ => this._validate_serialrx('serialrx_inverted', ['OFF']))
-          .then(_ => { if (!_) serialCheck.push('Serial Receiver UART is inverted! Hint: set serialrx_inverted = OFF') })
-          .then(_ => this._validate_serialrx('serialrx_halfduplex', ['OFF', 'AUTO']))
-          .then(_ => { if (!_) serialCheck.push('Serial Receiver UART is not in full duplex! Hint: set serialrx_halfduplex = OFF') })
-          .then(_ => this._validate_serialrx('rx_spi_protocol', ['EXPRESSLRS']))
-          .then(_ => { if (_) serialCheck.push('ExpressLRS SPI RX detected\n\nUpdate via betaflight to flash your RX\nhttps://www.expresslrs.org/2.0/hardware/spi-receivers/') })
-          .then(_ => {
-            if (serialCheck.length > 0) {
-              let msg = ''
-              this.log('[ERROR] Invalid serial RX configuration detected:\n')
-              for (const err of serialCheck) {
-                this.log('    !!! ' + err + ' !!!')
-                msg += err + '\n'
-              }
-              this.log('\n    Please change the configuration and try again!')
-              throw new AlertError('Invalid serial RX configuration detected', msg)
-            }
-          })
-          .then(_ => {
-            this.log('\nAttempting to detect FC UART configuration...')
-            return this.transport.write_string('serial\r\n')
-          })
-          .then(async _ => {
-            this.transport.set_delimiters(['\n'])
-            let index = false
-            while (true) {
-              const line = await this.transport.read_line({ timeout: 200 })
-              if (line === '') {
-                break
-              }
-              if (line.startsWith('serial')) {
-                const regexp = /serial (?<port>[0-9]+) 64 /
-                const config = line.match(regexp)
-                if (config && config.groups && config.groups.port) {
-                  index = config.groups.port
-                  break
-                }
-              }
-            }
-            if (!index) {
-              this.log('!!! RX Serial not found !!!!\n  Check configuration and try again...')
-              throw new AlertError('Serial RX not found', 'Check flight controller RX configuration')
-            }
-            return index
-          })
-          .then(index => this.transport.write_string(`serialpassthrough ${index} ${this.transport.baudrate}\r\n`))
-          .then(_ => this._sleep(200))
-          .then(async _ => {
-            try {
-              for (let i = 0; i < 10; i++) { await this.transport.read_line({ timeout: 200 }) }
-            } catch (e) {
-            }
-            this.log('======== PASSTHROUGH DONE ========')
-          })
-      })
+    await this.transport.write_string(`serialpassthrough ${index} ${this.transport.baudrate}\r\n`)
+    await this._sleep(200)
+
+    try {
+      for (let i = 0; i < 10; i++) { await this.transport.read_line({ timeout: 200 }) }
+    } catch (e) {
+    }
+    this.log('======== PASSTHROUGH DONE ========')
   }
 
-  reset_to_bootloader = () => {
+  reset_to_bootloader = async () => {
     this.log('======== RESET TO BOOTLOADER ========')
-    let p
+
     if (this.half_duplex) {
       this.log('Using half duplex (GHST)')
-      p = this.transport.write_array(Bootloader.get_init_seq('GHST'))
+      await this.transport.write_array(Bootloader.get_init_seq('GHST'))
     } else {
       this.log('Using full duplex (CRSF)')
       const train = new Uint8Array(32)
       train.fill(0x55)
-      p = this.transport.write_array(new Uint8Array([0x07, 0x07, 0x12, 0x20]))
-        .then(_ => this.transport.write_array(train))
-        .then(_ => this._sleep(200))
-        .then(_ => this.transport.rawRead({ timeout: 1 }))
-        .catch(_ => {})
-        .then(_ => this.transport.write_array(Bootloader.get_init_seq('CRSF')))
-        .then(_ => this._sleep(200))
+      await this.transport.write_array(new Uint8Array([0x07, 0x07, 0x12, 0x20]))
+      await this.transport.write_array(train)
+      await this._sleep(200)
+      try {
+        await this.transport.rawRead({ timeout: 1 })
+      } catch {}
+      await this.transport.write_array(Bootloader.get_init_seq('CRSF'))
+      await this._sleep(200)
     }
 
-    return p
-      .then(_ => {
-        this.transport.set_delimiters(['\n'])
-        return this.transport.read_line({ timeout: 200 })
+    this.transport.set_delimiters(['\n'])
+    const rxTarget = (await this.transport.read_line({ timeout: 200 })).trim()
+
+    console.log(`rxtarget ${rxTarget}`)
+
+    if (rxTarget === '') {
+      this.log('Cannot detect RX target, blindly flashing!')
+    } else if (this.uploadforce) {
+      this.log(`Force flashing ${this.flash_target}, detected ${rxTarget}`)
+    } else if (rxTarget.toUpperCase() !== this.flash_target.toUpperCase()) {
+      const e = await SwalMUI.fire({
+        icon: 'question',
+        title: 'Targets Mismatch',
+        text: `Wrong target selected your RX is '${rxTarget}', trying to flash '${this.flash_target}'`,
+        confirmButtonText: 'Flash anyway',
+        showCancelButton: true
       })
-      .then(_ => _.trim())
-      .then(_ => {
-        console.log('got string ' + _)
-        return _
-      })
-      .then(async rxTarget => {
-        console.log('rxtarget ' + rxTarget)
-        if (rxTarget === '') {
-          this.log('Cannot detect RX target, blindly flashing!')
-        } else if (this.uploadforce) {
-          this.log(`Force flashing ${this.flash_target}, detected ${rxTarget}`)
-        } else if (rxTarget.toUpperCase() !== this.flash_target.toUpperCase()) {
-          const e = await SwalMUI.fire({
-            icon: 'question',
-            title: 'Targets Mismatch',
-            text: `Wrong target selected your RX is '${rxTarget}', trying to flash '${this.flash_target}'`,
-            confirmButtonText: 'Flash anyway',
-            showCancelButton: true
-          })
-          if (e !== 'confirm') {
-            this.log(`Wrong target selected your RX is '${rxTarget}', trying to flash '${this.flash_target}'`)
-            throw new MismatchError()
-          }
-        } else if (this.flash_target !== '') {
-          this.log(`Verified RX target '${this.flash_target}'`)
-        }
-        this.log('======== BOOTLOADER ENGAGED ========')
-        return this._sleep(500)
-      })
+      if (e !== 'confirm') {
+        this.log(`Wrong target selected your RX is '${rxTarget}', trying to flash '${this.flash_target}'`)
+        throw new MismatchError()
+      }
+    } else if (this.flash_target !== '') {
+      this.log(`Verified RX target '${this.flash_target}'`)
+    }
+    this.log('======== BOOTLOADER ENGAGED ========')
+    return await this._sleep(500)
   }
 }
 
