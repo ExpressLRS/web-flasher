22d40a83cba95d848d30f5b8899719df288b9fdb
Paul Kendall <pkendall64@gmail.com>
2024-04-15 16:50:18 +1200

    Better version comparitor

--- src/js/index.js
+++ src/js/index.js
@@ -205,17 +205,28 @@ const checkProxy = async () => {
 }
 
 const compareSemanticVersions = (a, b) => {
-  const a1 = a.split(/[.-]/)
-  const b1 = b.split(/[.-]/)
-  const len = Math.min(a1.length, b1.length)
-  for (let i = 0; i < len; i++) {
-    const a2 = +a1[i] || 0
-    const b2 = +b1[i] || 0
-    if (a2 !== b2) {
-      return a2 > b2 ? 1 : -1
-    }
+  // Split versions and discriminators
+  const [v1, d1] = a.split('-')
+  const [v2, d2] = b.split('-')
+
+  // Split version sections
+  const v1Sections = v1.split('.')
+  const v2Sections = v2.split('.')
+
+  // Compare main version numbers
+  for (let i = 0; i < Math.max(v1Sections.length, v2Sections.length); i++) {
+    const v1Section = parseInt(v1Sections[i] || 0, 10)
+    const v2Section = parseInt(v2Sections[i] || 0, 10)
+
+    if (v1Section > v2Section) return 1
+    if (v1Section < v2Section) return -1
   }
-  return b1.length - a1.length
+
+  // If main versions are equal, compare discriminators
+  if (d1 && !d2) return 1 // v1 is greater if it has a discriminator
+  if (!d1 && d2) return -1 // v2 is greater if it has a discriminator
+  if (d1 && d2) return d1.localeCompare(d2) // Compare discriminators
+  return 0 // Versions are equal
 }
 
 const compareSemanticVersionsRC = (a, b) => {
