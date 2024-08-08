e64367055e7595e7bfd5acf450a2693ca384595c
Paul Kendall <pkendall64@gmail.com>
2024-08-08 16:44:13 +1200

    Start of Vue based flasher app

---
+++ src/components/HoverCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { VHover, VCard } from 'vuetify/components';
+</script>
+
+<template>
+    <v-hover v-slot:default="{ isHovering, props }">
+        <v-card v-bind="$attrs, props" :class="isHovering ? 'elevation-16' : undefined"  variant="tonal">
+            <template v-for="(_, scopedSlotName) in $slots" #[scopedSlotName]="slotData">
+                <slot :name="scopedSlotName" v-bind="slotData" />
+            </template>
+            <template v-for="(_, slotName) in $slots" #[slotName]>
+                <slot :name="slotName" />
+            </template>
+        </v-card>
+    </v-hover>
+</template>
---
+++ src/state.js
@@ -0,0 +1,7 @@
+import { reactive } from 'vue'
+
+export const store = reactive({
+  firmware: '',
+  targetType: ''
+
+})
