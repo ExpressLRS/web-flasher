f6962080a7a4839fb4efa00e6d43c59a2822e3d4
Paul Kendall <pkendall64@gmail.com>
2024-08-24 07:45:54 +1200

    show/hide on wifi passsword

--- src/components/WiFiSettingsInput.vue
+++ src/components/WiFiSettingsInput.vue
@@ -1,9 +1,16 @@
 <script setup>
+import {ref} from "vue";
+
 let ssid = defineModel('ssid')
 let password = defineModel('password')
+
+let showPassword = ref(false)
 </script>
 
 <template>
   <VTextField v-model="ssid" label="WiFi SSID"/>
-  <VTextField v-model="password" label="WiFi Password"/>
+  <VTextField v-model="password" label="WiFi Password"
+              :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="showPassword ? 'text' : 'password'"
+              @click:append-inner="showPassword = !showPassword"/>
 </template>
