fcfea0960d4aa10635acafca1d67cce5b8422a3d
Paul Kendall <pkendall64@gmail.com>
2024-08-28 20:25:18 +1200

    Nicer hover card image fading in/out

--- src/components/HoverCard.vue
+++ src/components/HoverCard.vue
@@ -7,16 +7,50 @@ defineProps(['image', 'hoverImage', 'title', 'text'])
 <template>
   <VHover v-slot:default="{ isHovering, props }">
     <VCard v-bind="$attrs, props" :class="isHovering ? 'elevation-16' : undefined" variant="tonal">
-      <VLabel/>
-      <VLayout align-center>
-        <VFadeTransition  mode="out-in" type="transition" duration="200">
-          <VImg :src="hoverImage" height="100px" width="100px" v-if="isHovering"/>
-          <VImg :src="image" height="100px" width="100px" v-else/>
-        </VFadeTransition>
-      </VLayout>
+      <div class="parent">
+        <img :src="image" height="100px" width="100px"/>
+        <img :src="hoverImage" height="100px" width="100px" :class="isHovering ? 'fadeInImage' : 'fadeOutImage'"/>
+      </div>
       <VCardTitle>{{ title }}</VCardTitle>
       <VCardText>{{ text }}
       </VCardText>
     </VCard>
   </VHover>
 </template>
+
+<style scoped>
+.parent {
+  padding-top: 20px;
+  display: flex;
+  justify-content: center;
+}
+
+@keyframes fadeOut {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+
+.fadeOutImage {
+  animation: fadeOut 1s forwards;
+  position: absolute;
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.fadeInImage {
+  animation: fadeIn 1s forwards;
+  position: absolute;
+}
+
+</style>
